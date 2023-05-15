9740270b04a9533b6efc7a5734530828ecf48258
TalesHernandes <119443598+TalesHernandes@users.noreply.github.com>
2023-05-15 11:26:00 +0000

    Get

--- src/Get.js
+++ src/Get.js
@@ -1,19 +1,18 @@
 export default function Get() {
     fetch("https://mack-webmobile.vercel.app/api/users")
     .then((response) => {
-      if (response.ok) {
+        if (response.ok) {
         return response.json();
-      } else {
+        } else {
         throw new Error("Get failed");
-      }
+        }
     })
     .then((data) => {
-      console.log(data);
-      // do something with the data
+        console.log(data);
+        // do something with the data
     })
     .catch((error) => {
-      console.error(error);
-      alert("Get failed");
+        console.error(error);
+        alert("Get failed");
     });
-  
-  }
+}
